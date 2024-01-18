215c0b8af41e3ddc08bf7a899c97b684452a4903
George Ionut Popa <alupop7602@ieselcaminas.org>
2024-01-18 20:12:53 +0100

    18-01-24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
-    implementation("androidx.compose.material:material-icons-extended:1.6.0-beta03")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0-rc01")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation ("androidx.compose.foundation:foundation:1.5.4")
@@ -68,6 +68,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("com.google.dagger:hilt-android:2.48.1")
+    implementation("com.square.retrofit2:retrofit:2.9.0")
     ksp("com.google.dagger:hilt-compiler:2.48.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
